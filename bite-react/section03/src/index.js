8f4b361f8e750872e1540a88a50e7d6572cf764c
KangwonPark <nambaggangwon@gmail.com>
2024-04-05 10:06:42 +0900

    240405

---
+++ bite-react/section03/src/index.js
@@ -0,0 +1,9 @@
+// const moduleData = require("./math.js");
+
+//구조분해할당
+
+import multi, { add, sub } from "./math.js";
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(multi(2, 3));
