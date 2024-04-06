238b24f6f522d0910c5f5dbf22c83debd80b0af8
KangwonPark <nambaggangwon@gmail.com>
2024-04-06 16:24:56 +0900

    라이브러리(모듈 삭제)

--- bite-react/section03/src/index.js
+++ bite-react/section03/src/index.js
@@ -1,9 +1,11 @@
-// const moduleData = require("./math.js");
 
-//구조분해할당
 
 import multi, { add, sub } from "./math.js";
+import randomColor from "randomcolor";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(multi(2, 3));
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(multi(2, 3));
