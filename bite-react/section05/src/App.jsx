d348709c45f03f3862e4f8f3b073ef861cc35efc
KangwonPark <nambaggangwon@gmail.com>
2024-04-14 10:49:39 +0900

    state로 상태관리 하기

--- bite-react/section05/src/App.jsx
+++ bite-react/section05/src/App.jsx
@@ -1,24 +1,37 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import { useState } from "react";
+//js 문법으로 변수를 만들어서 처리를 해도 되지않냐?
+// -> ㄴㄴ 그러면 리렌더링이 되지않아서 아무리 버튼을 눌러도 상태변화가 없음
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
+  //state는 useState를 이용해 생성한 값, setState는 state의 값을 변경하는 함수
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <Header />
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={() => {
+          setLight(light === "ON" ? "OFF" : "ON");
+        }}>{light === "ON" ? "끄기" : "켜기"}</button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          -
+        </button>
+      </div>
     </>
   );
 }
